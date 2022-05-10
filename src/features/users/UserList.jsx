056c3711bfc9cdf7392e6eb04c21684768f4cc1a
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-05-10 11:05:38 +0300

    add userlist-component

--- src/features/users/UserList.jsx
+++ src/features/users/UserList.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 
 export const UserList = () => {
-  const users = [];
+  const users = [
+    { id: "1", name: "Gökalp", email: "avcu219@gmail.com" },
+    { id: "2", name: "Nuri", email: "nuri1966@gmail.com" },
+  ];
 
-  const renderCard = () => <div>Users here</div>;
+  const renderCard = () =>
+    users.map((user) => (
+      <div className="bg-gray-300 p-5 flex items-center justify-between">
+        <div>
+        <h3 className="font-bold text-lg text-gray-700">{user.name}</h3> 
+        <span>{user.email}</span>
+        </div>
+        <div>
+
+        </div>
+      </div>
+    ));
   return (
     <div className="grid gap-5 md:grid-cols-2">
       {users.length ? ( // kullanıcıların bir değere sahip olup olmadığını kontrol ediyorum
