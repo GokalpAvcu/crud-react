093c450cdbbfc3ecf0d5494479c45d6dee7daae9
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-05-11 11:29:08 +0300

    add component

--- src/features/users/AddUser.jsx
+++ src/features/users/AddUser.jsx
@@ -2,30 +2,31 @@ import React from "react";
 import { Button } from "../../components/Button";
 import TextField from "../../components/TextField";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AddUser = () => {
+  const navigate = useNavigate();
   const [values, setValues] = useState({
     name: "",
     email: "",
   });
 
   const handleAddUser = () => {
-     setValues({ name:"", email:"",});
-     console.log(values);
-
-  }
+    setValues({ name: "", email: "" });
+    console.log(values);
+  };
   return (
     <div className="mt-10 max-w-xl mx-auto">
       <TextField
         label="Name"
         value={values.name}
-        onChange={(e) => setValues({...values, name: e.target.value})}
+        onChange={(e) => setValues({ ...values, name: e.target.value })}
         inputProps={{ type: "text", placeholder: "Enter the name" }}
       />
       <TextField
         label="Email"
         value={values.email}
-        onChange={(e) => setValues({...values, email: e.target.value})}
+        onChange={(e) => setValues({ ...values, email: e.target.value })}
         inputProps={{ type: "email", placeholder: "example@gmail.com" }}
       />
       <Button onClick={handleAddUser}>Submit</Button>
--- src/features/users/UserList.jsx
+++ src/features/users/UserList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import {Button } from "../../components/Button";
+import { Button } from "../../components/Button";
+import { Link } from "react-router-dom";
 
 export const UserList = () => {
   const users = [
@@ -9,28 +10,34 @@ export const UserList = () => {
 
   const renderCard = () =>
     users.map((user) => (
-      <div className="bg-gray-300 p-5 flex items-center justify-between">
+      <div
+        className="bg-gray-300 p-5 flex items-center justify-between"
+        key={user.id}
+      >
         <div>
           <h3 className="font-bold text-lg text-gray-700">{user.name}</h3>
           <span className="font-normal text-gray-600">{user.email}</span>
         </div>
         <div className="flex gap-2">
-          <button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-              />
-            </svg>
-          </button>
+          <Link to={`edit-user/${user.id}`}>
+            <button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+                />
+              </svg>
+            </button>
+          </Link>
+
           <button>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -51,10 +58,11 @@ export const UserList = () => {
       </div>
     ));
 
-
   return (
     <div>
-      <Button>Add User</Button>
+      <Link to="/add-user">
+        <Button>Add User</Button>{" "}
+      </Link>
       <div className="grid gap-5 md:grid-cols-2">
         {users.length ? ( // kullanıcıların bir değere sahip olup olmadığını kontrol ediyorum
           renderCard()
